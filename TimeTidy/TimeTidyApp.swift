d5b625d01990b5722c1c621989c846881fec1cfc
lananotrey <lananotrey@outlook.com>
2025-04-24 15:14:08 -0700

    Полная замена проекта TimeTidy

--- TimeTidy/TimeTidyApp.swift
+++ TimeTidy/TimeTidyApp.swift
@@ -1,1 +1,15 @@
-/Users/workcar/Documents/gen project/TimeTidy/TimeTidy/Generated/TimeTidy/TimeTidyApp.swift
+import SwiftUI
+import FirebaseCore
+import FirebaseRemoteConfig
+
+@main
+struct TimeTidyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            RemoteScreen()
+        }
+    }
+}
+
